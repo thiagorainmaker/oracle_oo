bdba76393175d81a97d2bdd73da7b3270da06d6d
thiago <thiago@thiago.thiago>
2018-10-30 12:22:07 -0300

    funções

--- .git_files_control/.teste.sql
+++ .git_files_control/.teste.sql
@@ -1,4 +1,7 @@
 
+-- arquivo de teste de metodos
+-- os testes só devem ser executados depois da criação do banco e da carga inicial
+
 -- teste método que retorna as matérias ordenadas por data
 SELECT a.manchete, a.data_escrita FROM tab_materia a ORDER BY a.lista_por_data();
 
